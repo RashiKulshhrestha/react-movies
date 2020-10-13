58bb3a543f3a8292306a92efcf2f2ed9e73ac721
Rashi Kulshrestha <rashikul5.rk@gmail.com>
2020-10-13 18:25:40 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectMongoDB = require('./config/db');
+const apiRouter = express.Router();
+const userRouter = require('./user/userRouter');
+
+app.use(cors());
+// Connect To MongoDB
+connectMongoDB();
+
+app.use(express.json({ extended: true }));
+
+//Define Routes
+apiRouter.use('/users/', userRouter);
+
+const port = process.env.PORT || 5000;
+  
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
